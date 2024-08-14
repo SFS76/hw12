5b965d5032c1724954f82ff95ba3436e9eb11f09
SFS76 <sfs76@yandex.ru>
2024-08-14 20:08:51 +0400

    start

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    String nameBook;
+    String author;
+    int publisherYear;
+
+    public Book (String nameBook, String author, int publisherYear) {
+        this.nameBook = nameBook;
+        this.author = author;
+        this.publisherYear = publisherYear;
+    }
+}

4603a9a69fb5943fd4fd5fb5ffa85a8885725105
SFS76 <sfs76@yandex.ru>
2024-08-14 20:08:51 +0400

    start

---
+++ src/Autor.java
@@ -0,0 +1,9 @@
+public class Autor {
+    String firstName;
+    String lastName;
+
+        public Autor (String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
